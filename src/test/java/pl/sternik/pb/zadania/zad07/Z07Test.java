a4cf1b497b39726b8fc2907701fbcc598beef940
Pawel Babiuch <pawel.babiuch@edu.uekat.pl>
2018-02-19 12:30:59 +0100

    #2 Dodany test dla zadania 7

--- src/test/java/pl/sternik/pb/zadania/zad07/Z07Test.java
+++ src/test/java/pl/sternik/pb/zadania/zad07/Z07Test.java
@@ -8,14 +8,20 @@ public class Z07Test {
 	@Test
 	public void testZnajdzPodzielna() throws Exception {
         int[] tablica = { 5, 4, 3, 4, 6, 6, 9, 1, 9 };
-        int dzielnik = 0;
-        //int dzielnik = 5;
-        
-        int excepted = -1;
-      //  int excepted = 0;
+        int dzielnik = 5;
+        int excepted = 0;
         int actual = Z07.znajdzPodzielna(tablica, dzielnik);
         
         assertThat(actual).isEqualTo(excepted);
 	}
 
+	@Test
+	public void testZnajdzPodzielnaZero() throws Exception {
+        int[] tablica = { 5, 4, 3, 4, 6, 6, 9, 1, 9 };
+        int dzielnik = 0;   
+        int excepted = Integer.MIN_VALUE;
+        int actual = Z07.znajdzPodzielna(tablica, dzielnik);
+        
+        assertThat(actual).isEqualTo(excepted);
+	}
 }
