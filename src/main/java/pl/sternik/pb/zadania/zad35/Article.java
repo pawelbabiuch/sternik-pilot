bca35d3af1c42f0d2d0b63d7ef26c2b1d9394725
Pawel Babiuch <pawel.babiuch@edu.uekat.pl>
2018-02-23 11:11:27 +0100

    update article Z35

--- src/main/java/pl/sternik/pb/zadania/zad35/Article.java
+++ src/main/java/pl/sternik/pb/zadania/zad35/Article.java
@@ -1,19 +1,62 @@
 ﻿package pl.sternik.pb.zadania.zad35;
 
-import java.io.FileNotFoundException;
-import java.util.Date;
-import java.text.DateFormat;
-import java.text.DecimalFormat;
-import java.util.Formatter;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*;
 
 //import org.mockito.cglib.core.Local;
 
 // Jest to uproszczona wersja Article dla zadania 35
 public class Article {
 
+	private static Scanner s = new Scanner(System.in);
+	
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    public Article(int id, String name, String description, double price) {
+        try{
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.price = price;
+        }catch(Exception e) {
+        	System.out.println(e.getMessage());
+        }
+    }
+    
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -53,59 +96,8 @@ public class Article {
 		return true;
 	}
 
-	private static Scanner s = new Scanner(System.in);
-	
-    private int id;
-    private String name;
-    private String description;
-    private double price;
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
     @Override
     public String toString() {
         return "Article [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + "]";
-    }
-    
-    public Article(int id, String name, String description, double price) {
-        try{
-            this.id = id;
-            this.name = name;
-            this.description = description;
-            this.price = price;
-        }catch(Exception e) {
-        	System.out.println(e.getMessage());
-        }
     }
 }
